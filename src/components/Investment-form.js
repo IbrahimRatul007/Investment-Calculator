734ff1d5a4cec5c0050a2b8234e370f95cc949f6
Ibrahim Ratul <ibrahim.ratul@brainstation-23.com>
2024-05-07 14:56:04 +0600

    added reset function and input value setting function

--- src/components/Investment-form.js
+++ src/components/Investment-form.js
@@ -1,16 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
 
 const InvestmentForm = () => {
+  const [inputValues, setInputValues] = useState({
+    currentSavings: 0,
+    yearlySavings: 0,
+    expectedInterests: 0,
+    investmentDuration: 0,
+  });
+
+  const resetForm = (e) => {
+    e.preventDefault();
+    setInputValues({
+      currentSavings: 0,
+      yearlySavings: 0,
+      expectedInterests: 0,
+      investmentDuration: 0,
+    });
+  };
+
+  const storeValues = (e) => {
+    e.preventDefault();
+    setInputValues((prevValue) => {
+      const id = e.target.id;
+      return { ...prevValue, [e.target.name]: e.target.value };
+      //   if (id == "current-savings") {
+      //     return { ...prevValue, currentSavings: e.target.value };
+      //   } else if (id == "yearly-contribution") {
+      //     return { ...prevValue, yearlySavings: e.target.value };
+      //   } else if (id == "expected-return") {
+      //     return { ...prevValue, expectedInterests: e.target.value };
+      //   } else if (id == "duration") {
+      //     return { ...prevValue, investmentDuration: e.target.value };
+      //   }
+    });
+  };
+  console.log(inputValues);
+
   return (
     <form className="form">
       <div className="input-group">
         <p>
           <label htmlFor="current-savings">Current Savings ($)</label>
-          <input type="number" id="current-savings" />
+          <input
+            name="currentSavings"
+            type="number"
+            id="current-savings"
+            value={inputValues.currentSavings}
+            onChange={storeValues}
+          />
         </p>
         <p>
           <label htmlFor="yearly-contribution">Yearly Savings ($)</label>
-          <input type="number" id="yearly-contribution" />
+          <input
+            name="yearlySavings"
+            type="number"
+            id="yearly-contribution"
+            value={inputValues.yearlySavings}
+            onChange={storeValues}
+          />
         </p>
       </div>
       <div className="input-group">
@@ -18,15 +65,27 @@ const InvestmentForm = () => {
           <label htmlFor="expected-return">
             Expected Interest (%, per year)
           </label>
-          <input type="number" id="expected-return" />
+          <input
+            name="expectedInterests"
+            type="number"
+            id="expected-return"
+            value={inputValues.expectedInterests}
+            onChange={storeValues}
+          />
         </p>
         <p>
           <label htmlFor="duration">Investment Duration (years)</label>
-          <input type="number" id="duration" />
+          <input
+            name="investmentDuration"
+            type="number"
+            id="duration"
+            value={inputValues.investmentDuration}
+            onChange={storeValues}
+          />
         </p>
       </div>
       <p className="actions">
-        <button type="reset" className="buttonAlt">
+        <button type="reset" className="buttonAlt" onClick={resetForm}>
           Reset
         </button>
         <button type="submit" className="button">
